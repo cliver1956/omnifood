6f4e82030a0611bbb7ed1f0bdc38275eaf29cd14
Clive <clive@reynoldscrc.me.uk>
2019-08-01 12:27:38 +0100

    Create mobile nav using jQuery

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-A Web Development Project from the Udemy Course 
-"Build Responsive Real World Websites with HTML5 and CSS3".
+Omnifood - A dummy website for a fictional food/meal delivery company.
+A Web Development Project from the Udemy Course "Build Responsive Real World Websites with HTML5 and CSS3".
+It introduces several cdn libraries along with some good jQuery effects.
--- resources/js/script.js
+++ resources/js/script.js
@@ -86,6 +86,24 @@ $('.js--wp-4').waypoint(function(direction) {
 }, {
     offset: '50%'
 });
+
+/* Mobile Navigation */
+
+$('.js--nav-icon').click(function() {
+    const nav = $('.js--main-nav');
+    const icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    };
+
+
+});
   
 
 });
